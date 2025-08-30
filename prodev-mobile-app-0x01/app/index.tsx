595cdae3400288208d7e39788a844f474f4f9830
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-08-30 12:51:45 +0100

    Fix formatting in index.tsx by consolidating Text components into a single line.

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -6,9 +6,7 @@ export default function Index() {
       <Text>Entry Screen - Awesome</Text>
 
       <View>
-  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-  <Text style={styles.smallText}>ALX is awesome</Text>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n  <Text style={styles.smallText}>ALX is awesome</Text>
 </View>
     </View>
   );
